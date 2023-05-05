6f0544a7c022836c55d6f8859fc334725b54f884
thanhtrung17072001 <thanhtrung.17072001@gmail.com>
2023-05-05 01:45:31 -0700

    test1

---
+++ sample/b64/base64_example.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "b64.h"
+void main()
+{   
+    char str[]="\"SSID\":\"HELLO\"";
+    printf("Original string: %s\n", str);
+    char *b64_str = jsontob64(str);
+    printf("Base64 string: [%s]\n", b64_str);
+    char *js_str =  b64tojson(b64_str);
+    printf("Decrypted String: [%s]\n", js_str);
+    free(b64_str);
+    free(js_str);
+}
